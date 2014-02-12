d5a339f7dc5f52434ed283b286137740eb64c1ff
Iker <iker@futureworkz.com>
2014-02-12 10:06:06 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Neember::Application.config.secret_key_base = '6235e7344d48f103e5eabcf6a1fda54e0a2bba558013bf1cfbc7886bc63554a9440b08418895f8c8ecebd00ec991769d0ffcc37337789a53a34247992a5b6a82'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Neember::Application.config.session_store :cookie_store, key: '_neember_session'
