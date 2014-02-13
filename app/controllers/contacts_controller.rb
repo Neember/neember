e2e27a8c58a70ad790e172dda558247f6c0bc7cd
Iker <iker@futureworkz.com>
2014-02-13 11:46:43 +0700

    Added Contacts#create

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,7 +2,25 @@ class ContactsController < ApplicationController
   def new
     @contact = Contact.new
   end
-  def create
 
+  def create
+    @contact = Contact.new(create_params)
+
+    if @contact.save
+      render :thankyou
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def create_params
+    params.require(:contact).permit(:name,
+                                    :email,
+                                    :phone,
+                                    :message,
+                                    :project_category,
+                                    :project_type)
   end
 end
--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -2,9 +2,37 @@ require ('spec_helper')
 
 describe ContactsController do
   describe '#new' do
-    it 'should display contact form' do
+    def do_request
       get :new
+    end
+
+    it 'should display contact form' do
+      do_request
       response.should render_template :new
+    end
+  end
+
+  describe '#create' do
+    def do_request
+      post :create, contact: contact_params
+    end
+
+    context 'Success' do
+      let(:contact_params) { attributes_for(:contact) }
+
+      it 'should save enquiry to database' do
+        expect { do_request }.to change(Contact, :count).by(1)
+        response.should render_template :thankyou
+      end
+    end
+
+    context 'failure' do
+      let(:contact_params) { attributes_for(:contact, email: '') }
+
+      it 'should not save enquiry to database' do
+        expect { do_request }.to change(Contact, :count).by(0)
+        response.should render_template :new
+      end
     end
   end
 end
