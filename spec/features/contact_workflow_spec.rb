57764dfd6a7aae278c205c55b3a78fbbddd51f61
Iker <iker@futureworkz.com>
2014-02-13 11:52:07 +0700

    Updated Capybara test for Contact

--- spec/features/contact_workflow_spec.rb
+++ spec/features/contact_workflow_spec.rb
@@ -6,7 +6,13 @@ describe 'Contact Us workflow' do
   it 'allow user to submit enquiry' do
     visit 'contacts/new'
     page.should have_content "Contact Us Form"
+    fill_in 'Name',             with: contact.name
+    fill_in 'Phone',            with: contact.phone
+    choose 'Web'
+    choose 'E-commerce'
+    click_on 'Send'
 
+    page.should have_content "can't be blank"
     fill_in 'Name',             with: contact.name
     fill_in 'Email',            with: contact.email
     fill_in 'Phone',            with: contact.phone
