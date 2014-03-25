3dd2efaaea7ed868fadfea60ea85f13c97ba5e2c
Steven Yap <stevenyap@futureworkz.com>
2014-03-25 10:48:38 +0700

    Added LayerSlider rotating banner

---
+++ vendor/assets/javascripts/layer_slider/layerslider.js
@@ -0,0 +1,4 @@
+//= require layer_slider/jQuery/jquery-easing-1.3
+//= require layer_slider/jQuery/jquery-transit-modified
+//= require layer_slider/js/layerslider.transitions
+//= require layer_slider/js/layerslider.kreaturamedia.jquery
